42c1235af2d3f9d80036ae333eb5a61c5fe20410
Tim <050508t@gmail.com>
2026-02-05 17:22:00 +0500

    moedl

---
+++ src/model/Task.java
@@ -0,0 +1,21 @@
+package model;
+
+public class Task {
+    private int id;
+    private String title;
+    private String descriptoin;
+    private TaskStatus status;
+
+    public Task(int id, String title, String descriptoin, TaskStatus status) {
+    this.id = id;
+    this.title = title;
+    this.descriptoin = descriptoin;
+    this.status = status;
+    }
+}
+
+@Override
+public String toString() {
+    return id + ". [" + status + "] " + title + ":" + description;
+   }
+}
---
+++ src/model/TaskStatus.java
@@ -0,0 +1,8 @@
+package model;
+
+public enum TaskStatus {
+    TODO,
+    IN_PROGRESS,
+    DONE
+
+}
